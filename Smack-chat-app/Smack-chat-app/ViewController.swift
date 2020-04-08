5ba90af341af02364f01058673e90f72a081e032
Priyank Ahuja <priyankahuja02@gmail.com>
2020-04-08 14:29:40 +0530

    initialise chat app

---
+++ Smack-chat-app/Smack-chat-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack-chat-app
+//
+//  Created by Pravir Ahuja on 08/04/20.
+//  Copyright © 2020 Priyank Ahuja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
