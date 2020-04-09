bd3ffac30a294cf9e2d1fb34df3a30d08ec5827c
Priyank Ahuja <priyankahuja02@gmail.com>
2020-04-09 16:41:33 +0530

    SWReveal

--- Smack-chat-app/Smack-chat-app/Controller/ChannelViewController.swift
+++ Smack-chat-app/Smack-chat-app/Controller/ChannelViewController.swift
@@ -1,20 +1,21 @@
 //
-//  ViewController.swift
+//  ChannelViewController.swift
 //  Smack-chat-app
 //
-//  Created by Pravir Ahuja on 08/04/20.
+//  Created by Pravir Ahuja on 09/04/20.
 //  Copyright © 2020 Priyank Ahuja. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChannelViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
---
+++ Smack-chat-app/Smack-chat-app/Controller/ChatViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ChatViewController.swift
+//  Smack-chat-app
+//
+//  Created by Pravir Ahuja on 09/04/20.
+//  Copyright © 2020 Priyank Ahuja. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var menuButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+
+}
