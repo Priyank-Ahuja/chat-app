94d5b911da688d8dddde568e4ecabb792da95787
Priyank Ahuja <priyankahuja02@gmail.com>
2020-04-11 01:06:28 +0530

    Generate a Avatar BG Color

--- Smack-chat-app/Smack-chat-app/Controller/CreateAccountViewController.swift
+++ Smack-chat-app/Smack-chat-app/Controller/CreateAccountViewController.swift
@@ -15,6 +15,8 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     
     //Actions
     @IBAction func closePressed(_ sender: Any) {
@@ -24,8 +26,12 @@ class CreateAccountViewController: UIViewController {
     //variables
     var avatarName = "profileDefault"
     var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    var bgColor: UIColor?
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
         guard let name = userNameText.text, userNameText.text != "" else { return }
         guard  let email = emailText.text, emailText.text != "" else {return}
         guard  let password = passwordText.text, passwordText.text != "" else {return}
@@ -36,8 +42,13 @@ class CreateAccountViewController: UIViewController {
                     if success {
                         AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor) { (success) in
                             if success {
+                                self.spinner.isHidden = true
+                                self.spinner.stopAnimating()
+                                
                                 print(UserDataService.instance.name, UserDataService.instance.avatarName)
                                 self.performSegue(withIdentifier: UNWIND, sender: nil)
+                                
+                                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
                             }
                         }
                     }
@@ -48,12 +59,21 @@ class CreateAccountViewController: UIViewController {
     @IBAction func pickAvatarPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_AVATAR_PICKER, sender: nil)
     }
+    
     @IBAction func pickBGColorPressed(_ sender: Any) {
+        let r = CGFloat(arc4random_uniform(255)) / 255
+        let g = CGFloat(arc4random_uniform(255)) / 255
+        let b = CGFloat(arc4random_uniform(255)) / 255
+
+        bgColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        UIView.animate(withDuration: 0.2) {
+            self.userImage.backgroundColor = self.bgColor
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
         // Do any additional setup after loading the view.
     }
     
@@ -61,7 +81,23 @@ class CreateAccountViewController: UIViewController {
         if UserDataService.instance.avatarName != "" {
             userImage.image = UIImage(named: UserDataService.instance.avatarName)
             avatarName = UserDataService.instance.avatarName
+            if avatarName.contains("light") && bgColor == nil {
+                userImage.backgroundColor = UIColor.lightGray
+            }
         }
+    }
+    
+    func setupView() {
+        spinner.isHidden = true
+        userNameText.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedString.Key.foregroundColor :  smackPurplePlaceHolder])
+        emailText.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedString.Key.foregroundColor :  smackPurplePlaceHolder])
+        passwordText.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedString.Key.foregroundColor :  smackPurplePlaceHolder])
+        let tap = UITapGestureRecognizer(target: self, action: #selector(CreateAccountViewController.handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap() {
+        view.endEditing(true)
     }
 
 }
--- Smack-chat-app/Smack-chat-app/View/RoundedButton.swift
+++ Smack-chat-app/Smack-chat-app/View/RoundedButton.swift
@@ -1,39 +1,40 @@
+////
+////  RoundedButton.swift
+////  Smack-chat-app
+////
+////  Created by Pravir Ahuja on 10/04/20.
+////  Copyright © 2020 Priyank Ahuja. All rights reserved.
+////
 //
-//  RoundedButton.swift
-//  Smack-chat-app
+//import UIKit
 //
-//  Created by Pravir Ahuja on 10/04/20.
-//  Copyright © 2020 Priyank Ahuja. All rights reserved.
-//
-
-import UIKit
-
 //@IBDesignable
-class TestRoundedButton: UIButton {
-
-//    @IBInspectable var mycornerRadius: CGFloat = 3.0 {
+//class TestRoundedButton: UIButton {
+//
+//    @IBInspectable var cornerRadius: CGFloat = 3.0 {
 //        didSet {
-//            self.layer.cornerRadius = mycornerRadius
+//            self.layer.cornerRadius = cornerRadius
 //        }
 //    }
 //
 //
-
+//
 //    override class func awakeFromNib() {
-//        super.awakeFromNib()
-//      self.layer.cornerRadius = 3.0
-//        //self.mycornerRadius = 3.0
+//       super.awakeFromNib()
+////      self.layer.cornerRadius = 3.0
+////        //self.mycornerRadius = 3.0
 //        setupViewTest()
 //    }
-//
-//    override class func prepareForInterfaceBuilder() {
-//        //super.prepareForInterfaceBuilder()
-//        //self.mycornerRadius = 3.0
-//        setupViewTest()
-//    }
-//
+////
+////    override class func prepareForInterfaceBuilder() {
+////        //super.prepareForInterfaceBuilder()
+////        //self.mycornerRadius = 3.0
+////        setupViewTest()
+////    }
+////
 //    func setupViewTest() {
-//        layer.cornerRadius =
+//        self.layer.cornerRadius = cornerRadius
+//        self.clipsToBounds = true
 //    }
-    
-}
+//    
+//}
