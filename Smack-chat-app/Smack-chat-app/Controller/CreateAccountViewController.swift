613ef5d3641ed0268721edd499f8992a0daf1301
Priyank Ahuja <priyankahuja02@gmail.com>
2020-04-10 14:35:16 +0530

    Registering a Use

--- Smack-chat-app/Smack-chat-app/Controller/CreateAccountViewController.swift
+++ Smack-chat-app/Smack-chat-app/Controller/CreateAccountViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
     
-    
+    //Actions
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard  let email = emailText.text, emailText.text != "" else {return}
+        guard  let password = passwordText.text, passwordText.text != "" else {return}
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+    }
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func pickBGColorPressed(_ sender: Any) {
     }
     
     override func viewDidLoad() {
---
+++ Smack-chat-app/Smack-chat-app/View/RoundedButton.swift
@@ -0,0 +1,39 @@
+//
+//  RoundedButton.swift
+//  Smack-chat-app
+//
+//  Created by Pravir Ahuja on 10/04/20.
+//  Copyright © 2020 Priyank Ahuja. All rights reserved.
+//
+
+import UIKit
+
+//@IBDesignable
+class TestRoundedButton: UIButton {
+
+//    @IBInspectable var mycornerRadius: CGFloat = 3.0 {
+//        didSet {
+//            self.layer.cornerRadius = mycornerRadius
+//        }
+//    }
+//
+//
+
+//    override class func awakeFromNib() {
+//        super.awakeFromNib()
+//      self.layer.cornerRadius = 3.0
+//        //self.mycornerRadius = 3.0
+//        setupViewTest()
+//    }
+//
+//    override class func prepareForInterfaceBuilder() {
+//        //super.prepareForInterfaceBuilder()
+//        //self.mycornerRadius = 3.0
+//        setupViewTest()
+//    }
+//
+//    func setupViewTest() {
+//        layer.cornerRadius =
+//    }
+    
+}
