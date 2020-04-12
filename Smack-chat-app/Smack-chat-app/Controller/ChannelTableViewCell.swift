2f1c2c9dcb29abb1a6d0897ab99a25589b116904
Priyank Ahuja <priyankahuja02@gmail.com>
2020-04-12 14:59:42 +0530

    Channels TableView

---
+++ Smack-chat-app/Smack-chat-app/Controller/ChannelTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  ChannelTableViewCell.swift
+//  Smack-chat-app
+//
+//  Created by Pravir Ahuja on 12/04/20.
+//  Copyright © 2020 Priyank Ahuja. All rights reserved.
+//
+
+import UIKit
+
+class ChannelTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+        // Configure the view for the selected state
+    }
+    
+    func configureCell(channel: Channel) {
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+    }
+}
--- Smack-chat-app/Smack-chat-app/Controller/ChannelViewController.swift
+++ Smack-chat-app/Smack-chat-app/Controller/ChannelViewController.swift
@@ -8,24 +8,14 @@
 
 import UIKit
 
-class ChannelViewController: UIViewController {
-
+class ChannelViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
-    
-    override func viewDidAppear(_ animated: Bool) {
-        setupUserInfo()
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(ChannelViewController.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
-        // Do any additional setup after loading the view.
-    }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
         if AuthService.instance.isLoggedIn {
@@ -36,7 +26,23 @@ class ChannelViewController: UIViewController {
             performSegue(withIdentifier: TO_LOGIN, sender: nil)
         }
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setupUserInfo()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelViewController.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        // Do any additional setup after loading the view.
+    }
+    
     @objc func userDataDidChange(_ notif: Notification) {
         setupUserInfo()
         
@@ -53,4 +59,24 @@ class ChannelViewController: UIViewController {
             userImage.backgroundColor = UIColor.clear
         }
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelTableViewCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    
 }
